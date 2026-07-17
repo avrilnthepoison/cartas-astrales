397b923abae8fb0a01aa6a313c0d924818d01f3c
Avril <115664856+avrilnthepoison@users.noreply.github.com>
2026-07-17 13:34:00 -0400

    0.6.4.0.10

--- script.js
+++ script.js
@@ -401,10 +401,10 @@ document.addEventListener("DOMContentLoaded", () => {
                     // Definición de aspectos con estilo (grosor, trazado, opacidad)
                     const aspectos = [
                         { tipo: 'conjuncion', angulo: 0, strokeWidth: 1.5, dasharray: null, opacity: 1 },
-                        { tipo: 'sextil', angulo: 60, strokeWidth: 2.25, dasharray: '5,5', opacity: 0.5 },
-                        { tipo: 'cuadratura', angulo: 90, strokeWidth: 2.25, dasharray: null, opacity: 0.5 },
-                        { tipo: 'trígono', angulo: 120, strokeWidth: 2.25, dasharray: '5,10', opacity: 0.5 },
-                        { tipo: 'oposicion', angulo: 180, strokeWidth: 3, dasharray: null, opacity: 1 }
+                        { tipo: 'sextil', angulo: 60, strokeWidth: 1.5, dasharray: '2,5', opacity: 0.5 },
+                        { tipo: 'cuadratura', angulo: 90, strokeWidth: 1.5, dasharray: null, opacity: 0.5 },
+                        { tipo: 'trígono', angulo: 120, strokeWidth: 1.5, dasharray: '5,10', opacity: 0.5 },
+                        { tipo: 'oposicion', angulo: 180, strokeWidth: 1.5, dasharray: null, opacity: 1 }
                     ];
 
                     for (const asp of aspectos) {
