044a6d0378e8f99f1346dc7234521bf68d37af59
Avril <115664856+avrilnthepoison@users.noreply.github.com>
2026-07-17 14:46:54 -0400

    0.6.4.1.6

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const RADIO_TEXTO_SIGNOS = 275;
     const RADIO_SIMBOLOS_DECANATOS = 277.5;
     const RADIO_GRADOS = 210;
-    const RADIO_ASPECTOS = 125;
+    const RADIO_ASPECTOS = 150;
 
     const nombresSignos = [
         "ARIES", "TAURO", "GÉMINIS", "CÁNCER",
